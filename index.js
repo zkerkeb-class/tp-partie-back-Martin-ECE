d727e1164af7f5e766c900297ab1664ca1df6319
Martin <martin..baussoon@edu.ece.fr>
2026-02-04 12:40:53 +0100

    Test

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
 import express from 'express';
 import pokemon from './schema/pokemon.js';
+import cors from 'cors';
+
 
 import './connect.js';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
