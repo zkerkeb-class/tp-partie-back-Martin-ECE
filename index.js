e39457945c78a4d596cf6107a9d5bf0125c0658c
Martin <martin..baussoon@edu.ece.fr>
2026-02-08 23:39:02 +0100

    V2

--- index.js
+++ index.js
@@ -24,6 +24,30 @@ app.get('/pokemons', async (req, res) => {
     }
 });
 
+// app.get("/pokemons", async (req, res) => {
+//   try {
+//     const page = parseInt(req.query.page) || 1;
+//     const limit = parseInt(req.query.limit) || 20;
+//     const skip = (page - 1) * limit;
+
+//     const total = await pokemon.countDocuments();
+//     const pokemons = await pokemon.find({})
+//       .skip(skip)
+//       .limit(limit)
+//       .sort({ id: 1 });
+
+//     res.json({
+//       page,
+//       totalPages: Math.ceil(total / limit),
+//       totalItems: total,
+//       items: pokemons,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Erreur serveur");
+//   }
+// });
+
 app.get('/pokemons/:id', async (req, res) => {
     try {
         const poke = await pokemon.findOne({ id: req.params.id });
@@ -36,19 +60,45 @@ app.get('/pokemons/:id', async (req, res) => {
         res.status(500).send(error.message);
     }
 });
-app.post("/pokemons", async (req, res) => {
-  try {
-    const lastPokemon = await pokemon.findOne().sort({ id: -1 });
-    const nextId = lastPokemon ? lastPokemon.id + 1 : 1;
 
-    const newPokemon = new pokemon({ ...req.body, id: nextId });
-    await newPokemon.save();
-    res.status(201).json(newPokemon);
+app.put("/pokemons/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    // Met à jour le Pokémon correspondant à l'id
+    const updated = await pokemon.findOneAndUpdate(
+      { id: Number(id) },  // id du Pokémon
+      updateData,
+      { new: true }        // retourne le Pokémon mis à jour
+    );
+
+    if (updated) {
+      res.json(updated);
+    } else {
+      res.status(404).send("Pokémon non trouvé");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erreur lors de la mise à jour du Pokémon");
+  }
+});
+
+app.delete("/pokemons/:id", async (req, res) => {
+  try {
+    const deletedPokemon = await pokemon.findOneAndDelete({
+      id: req.params.id,
+    });
+
+    if (!deletedPokemon) {
+      return res.status(404).send("Pokémon introuvable");
+    }
+
+    res.json({ message: "Pokémon supprimé avec succès" });
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
-
 
 
 app.listen(3000, () => {
